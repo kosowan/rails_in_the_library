02e51ec89f4b8c9bfba0e33d8abbea5145e2829a
Jesse <jkosowan2@academic.rrc.ca>
2022-10-11 18:00:53 -0500

    Added Book model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,8 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
 
-
+  validates :title, :rating, :reviews, :price, presence: true,
+  validates :title, uniqueness: true,
+  validates :rating, numericality: true,
+  validates :reviews, :price, numericality: {only_integer: true}
 end
