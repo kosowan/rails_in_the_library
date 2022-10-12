bd5822860138147c1c1470edc4a638f27ea80ffe
Jesse <jkosowan2@academic.rrc.ca>
2022-10-12 06:35:18 -0500

    added books, authors and genres controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/show'
+  get 'authors/index'
+  get 'authors/show'
+  get 'books/index'
+  get 'books/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
