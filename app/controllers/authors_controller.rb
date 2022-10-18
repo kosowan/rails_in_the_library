064f57b837d5ea700bbdb10a2d0a91f0c2628964
Jesse <jkosowan2@academic.rrc.ca>
2022-10-18 14:57:38 -0500

    styled home, genres, info, years views

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.all
+    @authors = Author.all.order("name ASC")
   end
 
   def show
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @books = Book.order("rating DESC").limit(50)
+    @books = Book.order("rating DESC").limit(250)
 
     @years = Year.order("year DESC")
+
+    @authors = Author.order("name ASC")
+
+    @genres = Genre.order("name ASC")
   end
 end
